bad852748aa83bfdd6861f549e3476ad44dd17a7
IuliaMStana <ramona_iulia2003@yahoo.com>
2024-04-19 19:31:49 +0300

    final project updates 2

--- src/test/java/utils/GenericUtils.java
+++ src/test/java/utils/GenericUtils.java
@@ -4,26 +4,6 @@ package utils;
             return ConfigUtils.getGenericElement(config, protocolPropertyName) +
                     "://" + ConfigUtils.getGenericElement(config, baseUrlPropertyName);
         }
-
-        public static String getDbHostname(String config, String propertyName) {
-            return ConfigUtils.getGenericElement(config, propertyName);
-        }
-
-        public static String getDbUser(String config, String propertyName) {
-            return ConfigUtils.getGenericElement(config, propertyName);
-        }
-
-        public static String getDbPassword(String config, String propertyName) {
-            return ConfigUtils.getGenericElement(config, propertyName);
-        }
-
-        public static String getDbSchema(String config, String propertyName) {
-            return ConfigUtils.getGenericElement(config, propertyName);
-        }
-
-        public static String getDbPort(String config, String propertyName) {
-            return ConfigUtils.getGenericElement(config, propertyName);
-        }
         public static String getBrowser(String config, String propertyName) {
             return ConfigUtils.getGenericElement(config, propertyName);
         }
